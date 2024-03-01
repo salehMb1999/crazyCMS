d56193e868f6dd9515f3c493f586800bcc459706
salehmb1999 <salehmirbagheri78@gmail.com>
2024-03-01 13:50:11 +0330

    some styles and 20% of courses panel logic

--- crazyFront/courses.js
+++ crazyFront/courses.js
@@ -1,1 +1,19 @@
 const $ = document;
+
+const addNewCourseBtn = $.getElementById("addNewCourse");
+const addCourseInModalBtn = $.getElementById("addCourseBtn");
+const modalBg = $.getElementById("modalBg");
+const addCourseModal = $.getElementById("addCourseModal");
+
+addNewCourseBtn.addEventListener("click", () => {
+  modalBg.classList.remove("hidden");
+  addCourseModal.classList.remove("hidden");
+  addCourseModal.classList.add("flex");
+});
+
+const closeModal = () => {
+  addCourseModal.classList.remove("flex");
+  modalBg.classList.add("hidden");
+  addCourseModal.classList.add("hidden");
+};
+modalBg.addEventListener("click", closeModal);
