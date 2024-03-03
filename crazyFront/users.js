abb2f3d0bc794c247f90a8f0ecc69ae1af78362c
salehmb1999 <salehmirbagheri78@gmail.com>
2024-03-03 06:04:51 +0330

    get main admin infos in users page

--- crazyFront/users.js
+++ crazyFront/users.js
@@ -19,6 +19,40 @@ const infosSectin = $.getElementById("infosSection");
 const coursesSection = $.getElementById("coursesSection");
 const sessionsSection = $.getElementById("sessionsSection");
 
+const topName = $.getElementById("topName");
+const topEmail = $.getElementById("topEmail");
+const sideUserName = $.getElementById("sideUserName");
+const sideEmail = $.getElementById("sideEmail");
+
+let mainAdminID = null;
+const getMainAdmin = () => {
+  mainAdminID = localStorage.getItem("loginId");
+  topName.innerHTML = "";
+  topEmail.innerHTML = "";
+  sideUserName.innerHTML = "";
+  sideEmail.innerHTML = "";
+  firstNameSide.innerHTML = "";
+  lastNameSide.innerHTML = "";
+
+  fetch(`http://localhost:3000/api/admins/${mainAdminID}`, {
+    method: "GET",
+  })
+    .then((res) => {
+      return res.json();
+    })
+    .then((admin) => {
+      console.log(admin);
+      topName.innerHTML = admin.firstName;
+      topEmail.innerHTML = admin.email;
+      sideUserName.innerHTML = admin.userName;
+      sideEmail.innerHTML = admin.email;
+      firstNameSide.innerHTML = admin.firstName;
+      lastNameSide.innerHTML = admin.lastName;
+    });
+};
+
+window.addEventListener("load", getMainAdmin);
+
 usersSection.addEventListener("click", () => {
   location.href = "http://127.0.0.1:5500/crazyFront/panel-users.html";
 });
